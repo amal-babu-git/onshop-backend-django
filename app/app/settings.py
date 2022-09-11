850492fb3dfc3ef1d9362af93dbee37e87c16c1c
Amal Babu <amalbabu1200@gmail.com>
2022-09-11 07:03:24 +0530

    hosts

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = ['api.onshop.amalbabudev.in', '127.0.0.1']
+ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
     filter(
         None,
